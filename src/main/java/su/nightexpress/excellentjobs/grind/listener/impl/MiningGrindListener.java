920a59f41859c301d5b8a91dba2ce363d4dbf845
Marshcl <notwyv@gmail.com>
2026-02-17 20:02:43 +0300

    Remove unused import for Sound class

--- src/main/java/su/nightexpress/excellentjobs/grind/listener/impl/MiningGrindListener.java
+++ src/main/java/su/nightexpress/excellentjobs/grind/listener/impl/MiningGrindListener.java
@@ -1,6 +1,5 @@
 package su.nightexpress.excellentjobs.grind.listener.impl;
 
-import org.bukkit.Sound;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
