975c111aff8f9346c03d8aff7e01c4a0d46f76cf
Marshcl <notwyv@gmail.com>
2026-02-17 19:45:00 +0300

    Remove unused constants from HookPlugin
    Removed unused constants for LevelledMobs and EvenMoreFish.

--- src/main/java/su/nightexpress/excellentjobs/hook/HookPlugin.java
+++ src/main/java/su/nightexpress/excellentjobs/hook/HookPlugin.java
@@ -5,8 +5,6 @@ public class HookPlugin {
     //public static final String ECONOMY_BRIDGE  = "EconomyBridge";
 
     public static final String MYTHIC_MOBS    = "MythicMobs";
-    public static final String LEVELLED_MOBS  = "LevelledMobs";
-    public static final String EVEN_MORE_FISH = "EvenMoreFish";
     public static final String CUSTOM_FISHING = "CustomFishing";
     public static final String CUSTOM_CROPS   = "CustomCrops";
 
